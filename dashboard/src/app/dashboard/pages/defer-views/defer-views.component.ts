aab8bd05f1290ed808578fc9208cfb2904bbe1e4
Iván Arenas Morante <arenasmorante@gmail.com>
2024-02-12 19:12:31 +0100

    pages

---
+++ dashboard/src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-views',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './defer-views.component.html',
+  styles: ``
+})
+export class DeferViewsComponent {
+
+}
