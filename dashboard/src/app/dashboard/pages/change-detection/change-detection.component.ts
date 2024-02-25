37937d2c2857b9e3fbe16fd2ea30cbdc2a74c809
Iván Arenas Morante <arenasmorante@gmail.com>
2024-02-25 19:17:38 +0100

    change detection

--- dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
     selector: 'app-change-detection',
     standalone: true,
-    imports: [CommonModule],
-    templateUrl: './change-detection.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, TitleComponent],
+    template: `
+
+    <app-title [titulo]="currentFramework()" />
+    <pre>{{ frameworkAsSignals() | json}}</pre>
+    <pre>{{ frameworkAsProperty | json}}</pre>
+
+    `,
     styles: ``,
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworkAsSignals().name}`
+  );
+
+  public frameworkAsSignals = signal({
+    name: 'Angular',
+    release: 2016,
+  });
+  public frameworkAsProperty = {
+    name: 'Angular',
+    release: 2016,
+  };
+
+  constructor(){
+    setTimeout(
+    () => {
+      this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignals.update( value => ({
+        ...value,
+        name: 'React'
+      })
+      )
+      console.log('Hecho')
+    },3000
+    );
+  }
+
+}
--- dashboard/src/app/shared/title/title.component.ts
+++ dashboard/src/app/shared/title/title.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute,  } from '@angular/core';
 
 @Component({
+  selector: 'app-title',
   standalone: true,
   imports: [CommonModule],
-  template: ``,
+  template: `<h1 class="text-3xl mb-5">{{title}} - {{withShadow}}</h1>`,
   styles: ``
 })
 export class TitleComponent {
+  @Input({required: true,alias: 'titulo'}) title: string = '';
+  @Input({ transform: booleanAttribute}) withShadow: boolean = false;
 
 }
